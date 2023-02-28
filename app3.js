7ca1fad58043ee5798a0287c8e09540f1b646e5a
cayetanogb22 <cayetanogb22@gmail.com>
2023-02-28 13:16:55 +0100

    Primer commit

---
+++ app3.js
@@ -0,0 +1,16 @@
+
+console.log('Inicio de programa'); // 1
+
+setTimeout(() => {
+    console.log('Primer Timeout'); // 5
+}, 3000);
+
+setTimeout(() => {
+    console.log('Segundo Timeout'); // 2 :: 3
+}, 0);
+
+setTimeout(() => {
+    console.log('Tercero Timeout'); // 3 :: 4
+}, 0);
+
+console.log('Fin de programa'); // 4 :: 2
